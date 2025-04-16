c52952a1c6f9d7f2cd86b19d7bcd1d3b7734fa32
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-04-16 12:33:57 +0200

    create react pckage

---
+++ Uebungen/firstreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h2>My Name is Dennis from Germany.</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ Uebungen/firstreact/src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <h1>Ich bin ein Footer</h1>
+    </div>
+  );
+}
+
+export default Footer;

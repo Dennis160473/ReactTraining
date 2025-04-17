90be5d9400876f16733afcc89838f4bdcd114e83
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-04-17 11:39:15 +0200

    Update App.js

--- Uebungen/firstreact/src/App.js
+++ Uebungen/firstreact/src/App.js
@@ -1,30 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const zahl = 15;
-  const name = "Suheib";
-  const element = <h3>Hallo aus dem H3 Tag</h3>;
+function Counter() {
+  // let zahl = 15
+  const [zahl, setZahl] = useState(15);
+  const [colour, setColour] = useState("red");
 
-  const meinStil = { backgroundColor: "blue", fontSize: "10px" }; // dieser Stil könnte als Variable übergeben werden an einem Style Attribut
-  function addieren(a, b) {
-    return a + b;
+  function reset() {
+    setZahl(0);
   }
-  return (
-    // <>...</> -> Fragment
-    <>
-      {/* Attribute werden in CamelCase Schreibweise geschrieben in JSX 
-styles müssen als Javascript Objekte übergeben werden, daher kommt die {{}} schreibweise zustande*/}
-      <h1 style={{ backgroundColor: "blue", fontSize: "50px" }}>
-        Hier eine Berechnung {zahl * 5}
-      </h1>
-      <p>Hallo aus dem P tag</p>
-      <h2>Hallo lieber {name}</h2>
-      {element}
 
-      <h3> Hier eine weitere Berechnung {addieren(7, 12)}</h3>
+  return (
+    <>
+      <p>Meine Lieblingszahl ist {zahl}</p>
+
+      <button onClick={() => setZahl(zahl + 1)}>+</button>
+      <button onClick={() => setZahl(zahl - 1)}>-</button>
+      <button onClick={reset}>Reset</button>
+
+      <h1 style={{ color: colour }}>Ich bin eine Überschrift</h1>
+      <button onClick={() => setColour("blue")}>
+        Ändere die Farbe zu blau
+      </button>
+      <button onClick={() => setColour("green")}>
+        Ändere die Farbe zu grün
+      </button>
     </>
   );
 }
 
-export default App;
+export default Counter;
